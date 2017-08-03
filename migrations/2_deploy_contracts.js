387398354ed8f51a2ca02e8ff52c5e3dc903b93f
Yuanfei Zhu <hi@yuanfeiz.cn>
2017-08-03 10:27:18 +0800

    Update constructor parameters in the deployment script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,10 @@
 var BodhiToken = artifacts.require("./BodhiToken.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(BodhiToken, 100, 500, web3.toWei(20e6), web3.eth.coinbase);
+  deployer.deploy(BodhiToken, 
+    100,  // startBlock
+    500,  // endBlock 
+    100,  // initialExchangeRate
+    web3.toWei(20e6), // presaleAmount
+    web3.eth.coinbase); // wallet
 };
