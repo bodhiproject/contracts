bf235682e7543ecff6b89d52ef2ab6e977345a25
Deric Walintukan <deric.walintukan@gmail.com>
2017-09-09 21:20:34 +0700

    Add pausable contract

---
+++ contracts/Pausable.sol
@@ -0,0 +1,37 @@
+pragma solidity ^0.4.11;
+
+import './Ownable.sol';
+
+/**
+ * @dev Base contract that allows a mechanism to stop defined pausable actions.
+ */
+contract Pausable is Ownable {
+	bool public paused = false;
+
+	// Events
+	event Pause();
+	event Unpause();
+
+	// Modifiers
+	modifier whenPaused() {
+		require(paused);
+		_;
+	}
+
+	modifier whenNotPaused() {
+		require(!paused);
+		_;
+	}
+
+	/// @notice Allows the owner to pause the contract
+	function pause() public onlyOwner whenNotPaused {
+		paused = true;
+		Pause();
+	}
+
+	/// @notice Allows the owner to unpause the contract
+	function unpause() public onlyOwner whenPaused {
+		paused = false;
+		Unpause();
+	}
+}
