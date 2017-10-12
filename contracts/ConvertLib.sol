9ba41314d85232954f1ff7a8e95b6dcb1d8045fd
Deric Walintukan <deric.walintukan@gmail.com>
2017-10-13 03:36:00 +0800

    Remove convertlib

--- contracts/ConvertLib.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.4.10;
-
-// Sida: no idea what this does, will keep it for now
-library ConvertLib {
-  function convert(uint amount, uint conversionRate) returns (uint convertedAmount) {
-    return amount * conversionRate;
-  }
-}
