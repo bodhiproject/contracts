40412f45b5054b32d6a9ca7ea2c796d2c900167c
Deric Walintukan <deric.walintukan@gmail.com>
2017-10-26 18:04:22 +0700

    Add test for allowance

--- test/standard_token.js
+++ test/standard_token.js
@@ -1,6 +1,5 @@
 const StandardTokenMock = artifacts.require('./mocks/StandardTokenMock.sol');
 const assert = require('chai').assert;
-const web3 = global.web3;
 
 contract('StandardToken', function(accounts) {
     const owner = accounts[0];
@@ -15,7 +14,25 @@ contract('StandardToken', function(accounts) {
         instance = await StandardTokenMock.new(...Object.values(tokenParams), { from: owner });
     });
 
-    it("should assert true", function(done) {
-        assert.isTrue(true);
+    describe('constructor', async function() {
+        it('should initialize all the values correctly', async function() {
+            assert.equal(await instance.balanceOf(owner, { from: owner }), tokenParams._initialBalance, 
+                'owner balance does not match');
+            assert.equal(await instance.totalSupply.call(), tokenParams._initialBalance, 'totalSupply does not match');
+        });
+    });
+
+    describe('allowance', async function() {
+        it('should return the right allowance', async function() {
+            let acct1Allowance = 1000;
+            await instance.approve(accounts[1], acct1Allowance, { from: owner });
+            assert.equal(await instance.allowance(owner, accounts[1]), acct1Allowance, 
+                'accounts[1] allowance does not match');
+
+            let acct2Allowance = 3000;
+            await instance.approve(accounts[2], acct2Allowance, { from: owner });
+            assert.equal(await instance.allowance(owner, accounts[2]), acct2Allowance, 
+                'accounts[2] allowance does not match');
+        });
     });
 });
